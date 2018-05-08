06a8f44e58b60b32ab7eb24c5bdfcbaff81c3a67
XGWang0 <xgwang@suse.com>
2018-05-08 17:02:16 +0800

    pypi upload myfunction test

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='wxgfuntest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/XGWang0/qa-myself/tree/master/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['wxgfuntest',],
+      zip_safe=False)
+
